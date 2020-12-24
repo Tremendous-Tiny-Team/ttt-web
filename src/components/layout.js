0a088126560e94230bf26062bbcc21f9b59351f8
Alex Kam <kavimaluskam@gmail.com>
2020-12-24 17:05:15 +0800

    feat: add theme control

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,12 +31,16 @@ const Layout = ({ children }) => {
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
+          color: "var(--color-text)",
+          background: "var(--color-background)",
         }}
       >
         <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
+        <footer
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
---
+++ src/components/themeSelect.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+import { ThemeContext } from "../contexts/ThemeContext"
+
+import { THEMES } from "../styles/themes"
+
+const ThemeSelect = () => {
+  const { colorMode, setColorMode } = React.useContext(ThemeContext)
+  return (
+    <select
+      value={colorMode}
+      onChange={event => setColorMode(event.target.value)}
+    >
+      {THEMES.map(theme => (
+        <option key={theme} value={theme}>
+          {theme}
+        </option>
+      ))}
+    </select>
+  )
+}
+
+export default ThemeSelect
