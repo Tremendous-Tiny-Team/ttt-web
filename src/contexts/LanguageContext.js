321c336984816b6a5fd1a5ab6f2b9c8794a27faa
Alex Kam <kavimaluskam@gmail.com>
2021-01-18 00:01:40 +0800

    feat: add language context

---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,52 @@
+import React, { createContext, useContext } from 'react';
+
+/** @type {React.Context<ContextValue>} */
+const LanguageContext = createContext({
+  currentLanguageCode: undefined,
+  defaultLanguageCode: undefined,
+  pathname: undefined,
+  languages: [],
+});
+
+/** @type {React.FC<ContextValue>} */
+const LanguageProvider = ({
+  currentLanguageCode,
+  defaultLanguageCode,
+  pathname,
+  languages,
+  children,
+}) => {
+  return (
+    <LanguageContext.Provider
+      value={{
+        currentLanguageCode,
+        defaultLanguageCode,
+        pathname,
+        languages,
+      }}
+    >
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+const useLanguage = () => {
+  const context = useContext(LanguageContext);
+  return context;
+};
+
+/**
+ * @typedef {Object} ContextValue
+ * @prop {string} currentLanguageCode
+ * @prop {string} defaultLanguageCode
+ * @prop {string} pathname
+ * @prop {Array<Language>} languages
+ */
+
+/**
+ * @typedef {Object} Language
+ * @prop {string} code
+ * @prop {string} path
+ */
+
+export { LanguageProvider, useLanguage };
