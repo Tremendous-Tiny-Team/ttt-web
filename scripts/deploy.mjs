2b76c91bb31c121f61b9e72096cb1df3943b3338
victor77dev <victor77.dev@gmail.com>
2021-01-20 22:00:25 +0100

    Handle build error

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -2,5 +2,9 @@ import {exec} from 'child_process';
 
 process.env.PATH_PREFIX = 'aaa';
 exec('gatsby build --prefix-paths', (error, stdout, stderr) => {
+  if (error || stderr) {
+    console.error(error);
+    process.exit(1);
+  }
   console.log(stdout);
 });
