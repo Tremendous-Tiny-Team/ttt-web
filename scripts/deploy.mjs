e093cabe5cfaab85308774fd072fcbba2e2bf6f5
victor77dev <victor77.dev@gmail.com>
2021-01-20 22:42:11 +0100

    Add deploy gh-pages

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -1,10 +1,25 @@
 import {exec} from 'child_process';
+import * as ghPages from 'gh-pages';
 
-process.env.PATH_PREFIX = 'aaa';
+const deployEnv = 'lab'
+process.env.PATH_PREFIX = deployEnv;
+
 exec('gatsby build --prefix-paths', (error, stdout, stderr) => {
   if (error || stderr) {
     console.error(error);
     process.exit(1);
   }
+
   console.log(stdout);
+
+  ghPages.publish('public', {
+    branch: 'public/gh-pages',
+    dest: deployEnv,
+    tag: deployVersion || '',
+    message: `Build from ${branch} ${commitHash}`,
+    user: {
+      name: authorName,
+      email: authorEmail,
+    },
+  });
 });
