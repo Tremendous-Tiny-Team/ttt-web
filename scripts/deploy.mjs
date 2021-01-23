31f55d734969432c53568fad1168313a12237be8
victor77dev <victor77.dev@gmail.com>
2021-01-23 21:43:34 +0100

    Prettier

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -1,10 +1,14 @@
-import {exec} from 'child_process';
+import { exec } from 'child_process';
 import * as ghPages from 'gh-pages';
 
 const branch = process.env.GITHUB_REF.replace('refs/heads/', '');
 console.log(branch);
-const deployEnv = branch.match(/^main|^release|^production|^hotfix|^lab-\d+/)?.[0];
-const deployVersion = deployEnv?.search(/release|production|hotfix/) ? branch.split('/')?.[0] : null;
+const deployEnv = branch.match(
+  /^main|^release|^production|^hotfix|^lab-\d+/
+)?.[0];
+const deployVersion = deployEnv?.search(/release|production|hotfix/)
+  ? branch.split('/')?.[0]
+  : null;
 
 process.env.PATH_PREFIX = deployEnv;
 
@@ -16,19 +20,23 @@ exec('gatsby build --prefix-paths', (error, stdout, stderr) => {
 
   console.log(stdout);
 
-  ghPages.publish('public', {
-    user: {
-      name: process.env.GITHUB_ACTOR,
-      email: 'victor77.dev@gmail.com',
+  ghPages.publish(
+    'public',
+    {
+      user: {
+        name: process.env.GITHUB_ACTOR,
+        email: 'victor77.dev@gmail.com',
+      },
+      repo: `https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`,
+      branch: 'public/gh-pages',
+      dest: deployEnv,
+      tag: deployVersion || '',
+      message: `Build from ${branch} ${process.env.GITHUB_SHA}`,
     },
-    repo: `https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`,
-    branch: 'public/gh-pages',
-    dest: deployEnv,
-    tag: deployVersion || '',
-    message: `Build from ${branch} ${process.env.GITHUB_SHA}`,
-  }, (data) => {
-    console.log(data);
-  });
+    (data) => {
+      console.log(data);
+    }
+  );
   console.log({
     user: {
       name: process.env.GITHUB_ACTOR,
