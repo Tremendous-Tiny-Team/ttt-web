0d857456fa983a4a948208b1d8edb1559430e322
victor77dev <victor77.dev@gmail.com>
2021-01-24 17:23:30 +0100

    Remove unnecessary

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { ThemeProvider } from './src/contexts/ThemeContext';
-
-export const wrapPageElement = ({ element }) => {
-  return <ThemeProvider>{element}</ThemeProvider>;
-};
